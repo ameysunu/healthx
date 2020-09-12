67ce99e0c9d0f9ce02c94fae179dc36317e83ba1
Amey Sunu <ameysunu00719@gmail.com>
2020-09-13 00:07:36 +0300

    ui changes

--- lib/user.dart
+++ lib/user.dart
@@ -16,42 +16,92 @@ class _UserState extends State<User> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Hexcolor('#FFDFCA'),
-                      backgroundImage: NetworkImage(imageUrl),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Hexcolor('#FFE8F7'),
+                  child: Row(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          name,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: 'Gotham',
-                              fontSize: 20),
+                        padding: const EdgeInsets.all(20.0),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Hexcolor('#FFE8F7'),
+                          backgroundImage: NetworkImage(imageUrl),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          email,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: 'Gotham',
-                              fontSize: 15),
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              name,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Gotham',
+                                  fontSize: 20),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              email,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Gotham',
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 0, 10, 10),
+                child: Card(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/walk.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.38,
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                "678/6000",
+                                style: TextStyle(
+                                    color: Hexcolor('#5C6178'),
+                                    fontFamily: 'Gotham',
+                                    fontSize: 40),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10.0, 0, 10, 10),
+                          child: Text(
+                            "Daily Steps",
+                            style: TextStyle(
+                                color: Hexcolor('#5C6178'),
+                                fontFamily: 'Gotham',
+                                fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
