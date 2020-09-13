772e57837743864c44e8ed6383dc72f7e9a4f707
Amey Sunu <ameysunu00719@gmail.com>
2020-09-13 13:57:11 +0300

    final ui changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:healthx/introduction.dart';
 import 'package:healthx/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Introduction());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
